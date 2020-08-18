b23a779ca683c48f0022661c8f75dcb8c2b2370f
Hamza Ahmed <Hamza Ahmed@IN0000318>
2020-08-17 20:18:34 -0400

    Started implementing form-based authentication, added a few comments on method
    Mostly read and researched on web-app security

--- test-web/src/Validation/CheckAuthentication.java
+++ test-web/src/Validation/CheckAuthentication.java
@@ -3,14 +3,19 @@ package Validation;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-
+/**
+ * 
+ * @author Hamza Ahmed
+ * This class is used to test the login function with hard-coded password and username
+ *  
+ */
 @SessionScoped
 @ManagedBean
 public class CheckAuthentication {
 
 	private String username;
 	private String password;
-	private boolean isLogin = false;
+	private boolean isLogin = false; //authentication check is done from this value
 		
 	public CheckAuthentication() {
 	
@@ -47,15 +52,22 @@ public class CheckAuthentication {
 		}
 	}
 	
+	/*
+	 * JSF saves the state onto the server, so in this method we will try to shift it to a new state
+	 * Whenever this method is called, and is successful, it would send a request to the specified URL
+	 */
 	public void doRedirect(String url) {
 		try {
-			FacesContext facesContext = FacesContext.getCurrentInstance();
-			facesContext.getExternalContext().redirect(url);
+			FacesContext facesContext = FacesContext.getCurrentInstance(); //facesContex stores current state of web-app
+			facesContext.getExternalContext().redirect(url); //sends a request to the server to redirect to the URL
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 	
+	/*
+	 * Hard-coded authentication, used for testing the web application
+	 */
 	public void checkAuth() {
 		if (username.equals("testuser") && 
 				password.equals("testpass")) {
